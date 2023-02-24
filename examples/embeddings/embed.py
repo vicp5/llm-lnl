bc88a38148350226b34ce439211f0ce86f368f93
vicp5 <vdiazyanez@pentalog.com>
2023-02-24 10:07:10 -0700

    added document embeddings example

---
+++ examples/embeddings/embed.py
@@ -0,0 +1,160 @@
+import numpy as np
+import openai
+import pandas as pd
+import tiktoken
+import os
+import time
+
+COMPLETIONS_MODEL = 'text-davinci-003'
+EMBEDDING_MODEL = 'text-embedding-ada-002'
+
+COMPLETIONS_API_PARAMS = {
+        "temperature": 0.0,
+        "max_tokens": 300,
+        "model": COMPLETIONS_MODEL,
+}
+
+MAX_SECTION_LEN = 800
+SEPARATOR = "\n* "
+ENCODING = "cl100k_base"  # encoding for text-embedding-ada-002
+
+encoding = tiktoken.get_encoding(ENCODING)
+separator_len = len(encoding.encode(SEPARATOR))
+
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def get_embedding(text: str, model: str = EMBEDDING_MODEL) -> list[float]:
+    time.sleep(0.25)  # avoid rate limit
+    result = openai.Embedding.create(
+            model=model,
+            input=text
+    )
+    return result["data"][0]["embedding"]
+
+
+def compute_doc_embeddings(df: pd.DataFrame) \
+        -> dict[tuple[str, str], list[float]]:
+    """
+    Create an embedding for each row in the dataframe using
+    the OpenAI Embeddings API. Return a dictionary that maps
+    between each embedding vector and the index of the row
+    that it corresponds to.
+    """
+    return {
+        idx: get_embedding(r.content) for idx, r in df.iterrows()
+    }
+
+
+def save_embeddings(fname: str,
+                    document_embeddings: dict[tuple[str, str], list[float]]
+                    ) -> None:
+    """
+    Write the document embeddings and their keys to a CSV.
+    fname is the path to a CSV with exactly these named columns:
+        "title", "heading", "0", "1",
+        ... up to the length of the embedding vectors.
+    """
+    df = pd.DataFrame(document_embeddings)
+    df = df.transpose()
+    df.rename(columns={'Unnamed: 0': 'title',
+                       'Unnamed: 1': 'heading'}, inplace=True)
+    df.to_csv(fname)
+
+
+def load_embeddings(fname: str) -> dict[tuple[str, str], list[float]]:
+    """
+    Read the document embeddings and their keys from a CSV.
+    fname is the path to a CSV with exactly these named columns:
+        "title", "heading", "0", "1",
+        ... up to the length of the embedding vectors.
+    """
+    df = pd.read_csv(fname, header=0)
+    max_dim = max([int(c) for c in df.columns
+                  if c != "title" and c != "heading"])
+    return {
+        (r.title, r.heading):
+        [r[str(i)] for i in range(max_dim + 1)] for _, r in df.iterrows()
+    }
+
+
+def vector_similarity(x: list[float], y: list[float]) -> float:
+    """
+    Returns the similarity between two vectors.
+    Because OpenAI Embeddings are normalized to length 1, the cosine
+    similarity is the same as the dot product.
+    """
+    return np.dot(np.array(x), np.array(y))
+
+
+def order_document_sections_by_query_similarity(
+        query: str, contexts: dict[(str, str), np.array]) \
+                -> list[(float, (str, str))]:
+    """
+    Find the query embedding for the supplied query, and compare
+    it against all of the pre-calculated document embeddings to find
+    the most relevant sections.
+    Return the list of document sections, sorted by relevance in descending
+    order.
+    """
+    query_embedding = get_embedding(query)
+    document_similarities = sorted([
+        (vector_similarity(query_embedding, doc_embedding),
+            doc_index) for doc_index, doc_embedding in contexts.items()],
+        reverse=True)
+    return document_similarities
+
+
+def construct_prompt(question: str,
+                     context_embeddings: dict, df: pd.DataFrame) -> str:
+    """
+    Fetch relevant
+    """
+    most_relevant_document_sections = \
+        order_document_sections_by_query_similarity(
+            question, context_embeddings)
+    chosen_sections = []
+    chosen_sections_len = 0
+    chosen_sections_indexes = []
+    for _, section_index in most_relevant_document_sections:
+        # Add contexts until we run out of space.
+        document_section = df.loc[section_index]
+        chosen_sections_len += document_section.tokens + separator_len
+        if chosen_sections_len > MAX_SECTION_LEN:
+            break
+        chosen_sections.append(SEPARATOR +
+                               document_section.content.replace("\n", " "))
+        chosen_sections_indexes.append(str(section_index))
+
+    # Useful diagnostic information
+    print(f"Selected {len(chosen_sections)} document sections:")
+    print("\n".join(chosen_sections_indexes))
+
+    header = """Answer the question as truthfully as possible using the """ \
+             """provided context, and if the answer is not contained """ \
+             """within the text below, say "I don't know."\n\nContext:\n"""
+    return header + "".join(chosen_sections) + "\n\n Q: " + question + "\n A:"
+
+
+def answer_query_with_context(
+        query: str,
+        df: pd.DataFrame,
+        document_embeddings: dict[(str, str), np.array],
+        show_prompt: bool = False
+        ) -> str:
+    prompt = construct_prompt(
+            query,
+            document_embeddings,
+            df
+            )
+
+    if show_prompt:
+        print(prompt)
+
+    response = openai.Completion.create(
+            prompt=prompt,
+            **COMPLETIONS_API_PARAMS
+            )
+
+    return response["choices"][0]["text"].strip(" \n")
---
+++ examples/embeddings/wiki.py
@@ -0,0 +1,206 @@
+import pandas as pd
+import wikipedia
+import re
+from typing import Set
+from transformers import GPT2TokenizerFast
+from nltk.tokenize import sent_tokenize
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+
+MAX_DEPTH_LEVEL = 3
+
+
+def count_tokens(text: str) -> int:
+    """count the number of tokens in a string"""
+    return len(tokenizer.encode(text))
+
+
+def reduce_long(
+        long_text: str, long_text_tokens: bool = False, max_len: int = 590
+) -> str:
+    """
+    Reduce a long text to a maximum of `max_len` tokens
+    by potentially cutting at a sentence end
+    """
+    if not long_text_tokens:
+        long_text_tokens = count_tokens(long_text)
+    if long_text_tokens > max_len:
+        sentences = sent_tokenize(long_text.replace("\n", " "))
+        ntokens = 0
+        for i, sentence in enumerate(sentences):
+            ntokens += 1 + count_tokens(sentence)
+            if ntokens > max_len:
+                return ". ".join(sentences[:i][:-1]) + "."
+
+    return long_text
+
+
+def filter_titles(titles, keywords):
+    """
+    Get the titles which are related, given a list of titles
+    """
+    titles = [title for title in titles if
+              any(keyword.lower() in title.lower()
+                  for keyword in keywords)]
+    return titles
+
+
+def get_option_by_keywords(options, keywords):
+    processed_options = [option for option in options if
+                         any(keyword.lower() in option.lower()
+                             for keyword in keywords)]
+    if len(processed_options) < 1:
+        return None
+
+    return processed_options[0]
+
+
+def get_wiki_page(title, keywords, depth_level=1):
+    """
+    Get the wikipedia page given a title
+    """
+    try:
+        return wikipedia.page(title)
+    except wikipedia.exceptions.DisambiguationError as e:
+        options_titles = filter_titles(e.options, keywords)
+        option = get_option_by_keywords(options_titles, keywords)
+        if option and depth_level < MAX_DEPTH_LEVEL:
+            return get_wiki_page(option, keywords, depth_level+1)
+        else:
+            return None
+    except wikipedia.exceptions.PageError:
+        return None
+
+
+def recursively_find_all_pages(
+        titles, keywords, titles_so_far=set(), depth_level=1):
+    """
+    Recursively find all the pages that are linked to
+    the Wikipedia titles in the list
+    """
+    all_pages = []
+    if depth_level == MAX_DEPTH_LEVEL:
+        return all_pages
+    titles = list(set(titles) - titles_so_far)
+    titles = filter_titles(titles, keywords)
+    titles_so_far.update(titles)
+    for title in titles:
+        page = get_wiki_page(title, keywords)
+        if page is None:
+            continue
+        print(f"downloaded wikipedia page: {title}")
+        all_pages.append(page)
+
+        new_pages = recursively_find_all_pages(
+                page.links, keywords, titles_so_far, depth_level+1)
+        for pg in new_pages:
+            if pg.title not in [p.title for p in all_pages]:
+                all_pages.append(pg)
+        titles_so_far.update(page.links)
+    return all_pages
+
+
+discard_categories = ['See also', 'References', 'External links',
+                      'Further reading', "Footnotes", "Bibliography",
+                      "Sources", "Citations", "Literature", "Footnotes",
+                      "Notes and references", "Photo gallery", "Works cited",
+                      "Photos", "Gallery", "Notes", "References and sources",
+                      "References and notes",]
+
+
+def clean_wiki_contents(
+        headings: str,
+        contents: str,
+        discard_categories: Set[str] = discard_categories,
+) -> tuple[[str], [str]]:
+    # discard the discard categories, accounting for a tree structure
+    max_level = 100
+    keep_group_level = max_level
+    remove_group_level = max_level
+    nheadings, ncontents = [], []
+    for heading, content in zip(headings, contents):
+        plain_heading = " ".join(heading.split(" ")[1:-1])
+        num_equals = len(heading.split(" ")[0])
+        if num_equals <= keep_group_level:
+            keep_group_level = max_level
+
+        if num_equals > remove_group_level:
+            if (
+                num_equals <= keep_group_level
+            ):
+                continue
+        keep_group_level = max_level
+        if plain_heading in discard_categories:
+            remove_group_level = num_equals
+            keep_group_level = max_level
+            continue
+        nheadings.append(heading.replace("=", "").strip())
+        ncontents.append(content)
+        remove_group_level = max_level
+    return (nheadings, ncontents)
+
+
+def extract_wiki_headings_contents(
+        wiki_text: str,
+        discard_categories: Set[str] = discard_categories,
+) -> tuple[str, str]:
+    # find all headings and the coresponding contents
+    headings = re.findall("==+ .* ==+", wiki_text)
+    for heading in headings:
+        wiki_text = wiki_text.replace(heading, "==+ !! ==+")
+    contents = wiki_text.split("==+ !! ==+")
+    contents = [c.strip() for c in contents]
+    assert len(headings) == len(contents) - 1
+    return (headings, contents)
+
+
+def extract_wiki_sections(
+        wiki_text: str,
+        title: str,
+        max_len: int = 1500,
+        discard_categories: Set[str] = discard_categories,
+) -> str:
+    """
+    Extract the sections of a Wikipedia page, discarding the
+    references and other low information sections
+    """
+    if len(wiki_text) == 0:
+        return []
+
+    headings, contents = extract_wiki_headings_contents(wiki_text)
+    cont = contents.pop(0).strip()
+    outputs = [(title, "Summary", cont, count_tokens(cont)+4)]
+
+    nheadings, ncontents = clean_wiki_contents(headings, contents)
+
+    # count the tokens of each section
+    ncontent_ntokens = [
+        count_tokens(c)
+        + 3
+        + count_tokens(" ".join(h.split(" ")[1:-1]))
+        - (1 if len(c) == 0 else 0)
+        for h, c in zip(nheadings, ncontents)
+    ]
+
+    # Create a tuple of (title, section_name, content, number of tokens)
+    outputs += [(title, h, c, t) if t < max_len
+                else (title, h, reduce_long(c, max_len),
+                      count_tokens(reduce_long(c, max_len)))
+                for h, c, t in zip(nheadings, ncontents, ncontent_ntokens)]
+    return outputs
+
+
+def tokenize_wikipedia_pages(fname, titles, keywords):
+    print("recursively downloading pages")
+    pages = recursively_find_all_pages(titles, keywords)
+    print("extracting tokenized sections")
+    res = []
+    for page in pages:
+        res += extract_wiki_sections(page.content, page.title)
+    print(f"backing up csv to {fname}")
+    df = pd.DataFrame(res, columns=['title', 'heading', 'content', 'tokens'])
+    df = df[df.tokens > 40]
+    df = df.drop_duplicates(['title', 'heading'])
+    df = df.reset_index().drop('index', axis=1)
+    df.to_csv(fname, index=False)
+    return df
